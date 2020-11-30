d2bb3f25a4cbc45a77425c5ddffb9ec08e6cf2f2
Paul Schrimpf <paul.schrimpf@gmail.com>
2020-11-30 07:44:33 -0800

    Updated dependencies to latest versions. Changed Flux usage to match updates in Flux.

--- docs/makeweave.jl
+++ docs/makeweave.jl
@@ -1,4 +1,8 @@
 using NeuralNetworkEconomics
+docdir = normpath(joinpath(dirname(Base.pathof(NeuralNetworkEconomics)), "..","docs"))
+using Pkg
+Pkg.activate(docdir)
+Pkg.instantiate()
 
 runweave= "weave" ∈ ARGS
 runnotebook= "notebook" ∈ ARGS
@@ -8,14 +12,20 @@ src=ARGS[1]
 if runweave
   println("weaving markdown for $src")
   using Weave
+  if !isdir("md")
+    mkdir("md")
+  end
   weave(src,out_path="md",
         cache=:refresh, cache_path="weavecache",
         doctype="github", mod=Main,
         args=Dict("md" => true))
 end
 
-if runnotebook
+if runnotebook  
   println("weaving notebook for $src")
+  if !isdir("build")
+    mkdir("build")
+  end  
   using Weave
   notebook(src, out_path=joinpath(pwd(),"build"), nbconvert_options="--allow-errors")
 end
